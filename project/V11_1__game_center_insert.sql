1ce2d0a8a1bb5f6575965744fb95a0318048b5be
Robert <rrmukhtarov@stud.kpfu.ru>
2020-12-22 15:06:32 +0300

    game_center + address data

---
+++ project/V11_1__game_center_insert.sql
@@ -0,0 +1,5 @@
+INSERT INTO `game_center` (`id`, `email`, `phone_number`, `address_id`) VALUES ('1', 'mail@gamerparadise.com', '78431234567', '1');
+INSERT INTO `game_center` (`id`, `email`, `phone_number`, `address_id`) VALUES ('2', 'info@1337games.com', '78431337123', '2');
+INSERT INTO `game_center` (`id`, `email`, `phone_number`, `address_id`) VALUES ('3', 'support@gamerparadise.com', '78437654321', '3');
+INSERT INTO `game_center` (`id`, `email`, `phone_number`, `address_id`) VALUES ('4', 'mail@retroentertainment.com', '74951234567 ', '4');
+INSERT INTO `game_center` (`id`, `email`, `phone_number`, `address_id`) VALUES ('5', 'info@gamechangers.com', '78437777777', '5');
---
+++ project/V11__address_insert.sql
@@ -0,0 +1,5 @@
+INSERT INTO `address` (`id`, `city`, `street`, `house`, `latitude`, `longitude`) VALUES ('1', 'Kazan', 'Leningradskaya', '70', '55.870240', '49.08743');
+INSERT INTO `address` (`id`, `city`, `street`, `house`, `latitude`, `longitude`) VALUES ('2', 'Kazan', 'Maksimova', '1', '55.859841', '49.097718');
+INSERT INTO `address` (`id`, `city`, `street`, `house`, `latitude`, `longitude`) VALUES ('3', 'Kazan', 'Baumana', '60', '55.790263', '49.115666');
+INSERT INTO `address` (`id`, `city`, `street`, `house`, `latitude`, `longitude`) VALUES ('4', 'Moscow', 'Arbat', '42', '55.748348', '37.586898');
+INSERT INTO `address` (`id`, `city`, `street`, `house`, `latitude`, `longitude`) VALUES ('5', 'Kazan', 'Gagrina', '105', '55.837026', '49.074079');
