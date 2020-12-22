3f71bdd3ec1ec656317fbb0954eeca4f924a03bb
Robert <rrmukhtarov@stud.kpfu.ru>
2020-12-22 17:26:19 +0300

    position update

--- project/V3__position_create.sql
+++ project/V3__position_create.sql
@@ -3,5 +3,4 @@ CREATE TABLE `position` (
   `room_id` INT UNSIGNED NOT NULL,
   `player_id` INT UNSIGNED NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `room_id_UNIQUE` (`room_id` ASC) VISIBLE,
   UNIQUE INDEX `player_id_UNIQUE` (`player_id` ASC) VISIBLE);
